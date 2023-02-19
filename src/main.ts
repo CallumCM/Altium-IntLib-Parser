548ea2763ec2347ed4e4f619bafea930f17a7d68
Turnip1234 <51388022+Turnip1234@users.noreply.github.com>
2023-02-19 12:33:57 -0800

    Add more example IntLib files

--- src/main.ts
+++ src/main.ts
@@ -10,68 +10,78 @@ const compoundDocumentIdentifier = buffer.subarray(0, 8).toString("hex");
 if (compoundDocumentIdentifier !== "d0cf11e0a1b11ae1") {
   throw new Error("Invalid Compound Document File");
 } else {
-  const doc = new OleDoc("./example.IntLib");
+  const doc = new OleDoc("./example3.IntLib");
 
   doc.on("error", (err) => {
     console.error(err);
   });
 
   doc.on("ready", () => {
-    let chunks = [];
 
     let IntLibJSON = {};
 
-    type Parameters = {
-      [key: string]: string;
-    }
-
-    let parameters: Parameters = {};
-    let pcblib: Parameters = {};
+    let parameters: object = {};
+    let pcblib: object = {};
 
     const parameterStream = doc.stream('Parameters   .bin')
-    parameterStream.on('data', function(chunk) { chunks.push(chunk); });
+    const parameterChunks = [];
+
+    parameterStream.on('data', function(chunk) { parameterChunks.push(chunk); });
     parameterStream.on('error', function(error) { console.error(error); });
 
     parameterStream.on('end', function() {
-      const buffer = Buffer.concat(chunks);
+      const buffer = Buffer.concat(parameterChunks);
 
       // Remove null bytes from the buffer
       const cleanBuffer = buffer.filter(byte => byte !== 0);
 
-      cleanBuffer.toString().slice(2).split('|').forEach(parameter => {
-        const parameterDict = parameter.split('=');
+      cleanBuffer.toString().slice(2, -14).split('|').forEach(parameter => {
+        const parameterDict = parameter.split('=', 1);
         parameters[parameterDict[0]] = parameterDict[1];
       });
 
-      if ('Pin Count' in parameters && parameters['Pin Count'].includes("\x16Height")) {
-
-        // Remove the trailing "\x16Height" from the Pin Count
-        parameters['Pin Count'] = parameters['Pin Count'].slice(0, -7);
+      if ('Pin Count' in parameters) {
+        const pinCountAndHeight = (parameters['Pin Count'] as string).replace('', '');
+        console.log(pinCountAndHeight);
+        parameters['Pin Count'] = {
+          'Pin Count': pinCountAndHeight[0],
+          'Height': pinCountAndHeight[1]
+        }
       }
 
-      console.log(parameters);
+      if ('Pad Count' in parameters) {
+        const padCountAndHeight = (parameters['Pad Count'] as string).replace('', '');
+        parameters['Pad Count'] = {
+          'Pad Count': padCountAndHeight[0],
+          'Height': padCountAndHeight[1]
+        }
+      }
 
       IntLibJSON['Parameters'] = parameters;
       console.log(IntLibJSON);
     });
 
-    /*const pcblibStream = doc.storage('PCBLib').stream('0.pcblib');
-    pcblibStream.on('data', function(chunk) { chunks.push(chunk); });
+    const pcblibStream = doc.storage('PCBLib').stream('0.pcblib');
+    const pcblibChunks = [];
+
+    pcblibStream.on('data', function(chunk) { pcblibChunks.push(chunk); });
     pcblibStream.on('error', function(error) { console.error(error); });
   
     pcblibStream.on('end', function() {
-      const buffer = Buffer.concat(chunks);
+      const buffer = Buffer.concat(pcblibChunks);
 
       // Remove null bytes from the buffer
       const cleanBuffer = buffer.filter(byte => byte !== 0);
 
-      cleanBuffer.toString().forEach(line => {
+      cleanBuffer.toString().split('|').forEach(line => {
         const lineDict = line.split('=');
         pcblib[lineDict[0]] = lineDict[1];
       });
 
-      //console.log(pcblib);
-    });*/
+      IntLibJSON['PCBLib'] = pcblib;
+
+      //console.log(IntLibJSON);
+    });
   });
 
   doc.read();
